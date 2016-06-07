51542e771dd4e03e7338475271a6783282ca41f0
koolkarni <akhilvallabh@gmail.com>
2016-06-06 18:59:38 -0700

    image resize

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+//var_dump($_FILES['uploaded_files']['tmp_name']);
+syslog(LOG_WARNING, "Request came");
+require_once 'google/appengine/api/cloud_storage/CloudStorageTools.php';
+use google\appengine\api\cloud_storage\CloudStorageTools;
+syslog(LOG_WARNING, "Imported Cloud Storage Tools");
+//var_dump( $_GET);
+$object_url=$_GET["image"];
+$size=intval($_GET["size"]);
+syslog(LOG_WARNING, "Object URL $object_url");
+syslog(LOG_WARNING, "Size $size");
+
+$bucket="gs://YOUR-PROJECT-ID.appspot.com/bucket_name/";
+$object_image_url = CloudStorageTools::getImageServingUrl($object_url,['size' => $size, 'crop' => false]);
+syslog(LOG_WARNING, "Output Url $object_image_url");
+header("location: $object_image_url");
+
+closelog();
+?>
